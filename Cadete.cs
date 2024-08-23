b3e52f5419bacfdfd4c5e80e33c969dc5062eb6e
Diego Cortez <dc936572@gmail.com>
2024-08-23 19:24:36 -0300

    creacion del metodo pedidosDespachados

--- Cadete.cs
+++ Cadete.cs
@@ -4,10 +4,10 @@ public class Cadete
     int Id;
     string Nombre;
     string Direccion;
-    int Telefono;
+    string Telefono;
     List<Pedido> listaPedido;
-
-    public Cadete(int id, string nombre, string direccion, int telefono, List<Pedido> listaPedido)
+    
+    public Cadete(int id, string nombre, string direccion, string telefono, List<Pedido> listaPedido)
     {
         Id = id;
         Nombre = nombre;
@@ -19,6 +19,19 @@ public class Cadete
     public int Id1 { get => Id; set => Id = value; }
     public string Nombre1 { get => Nombre; set => Nombre = value; }
     public string Direccion1 { get => Direccion; set => Direccion = value; }
-    public int Telefono1 { get => Telefono; set => Telefono = value; }
+    public string Telefono1 { get => Telefono; set => Telefono = value; }
     public List<Pedido> ListaPedido { get => listaPedido; set => listaPedido = value; }
+
+    //metodos
+    public int pedidosDespachados(List<Pedido> Listapedidos){
+       int totalPagar=0;
+       foreach (var pedido in Listapedidos)
+       {
+        if (pedido.Estado == 1)
+        {
+            totalPagar += 500;
+        }
+       }
+       return totalPagar;
+    }
 }
