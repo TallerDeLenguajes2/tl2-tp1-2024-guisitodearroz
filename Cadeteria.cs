d97ad65b042cd085a3d415ff26bbdac7d8df672f
Diego Cortez <dc936572@gmail.com>
2024-08-29 22:25:41 -0300

    cambios en cadeteria

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -1,18 +1,68 @@
 namespace cadeteria;
 public class Cadeteria
 {
-    string Nombre;
-    int Telefono;
-    List<Cadete> listadoCadetes;
+    private string nombre;
+    private string telefono;
+    private List<Cadete> listadoCadetes;
     
-    public Cadeteria(string nombre, int telefono, List<Cadete> listadoCadetes)
+    public Cadeteria(string nombre, string telefono, List<Cadete> listadoCadetes)
     {
-        Nombre = nombre;
-        Telefono = telefono;
+        this.nombre=nombre;
+        this.telefono=telefono;
         this.listadoCadetes = listadoCadetes;
     }
+    //geter u setter
+    public string Nombre{
+        get => nombre;
+        set => nombre= value;
+    }
+    public string Telefono{
+        get => telefono;
+        set => telefono= value;
+    }
+    public List<Cadete> ListadoCadetes{
+        get => listadoCadetes;
+        set => listadoCadetes= value;
+    }
 
-    public string Nombre1 { get => Nombre; set => Nombre = value; }
-    public int Telefono1 { get => Telefono; set => Telefono = value; }
-    public List<Cadete> ListadoCadetes { get => listadoCadetes; set => listadoCadetes = value; }
+    //metodos
+    public void cargarCadete(Cadete cadete){
+        listadoCadetes.Add(cadete);
+    }
+    public void borrarCadetePorId(int idCadete){
+        Cadete cadeteEncontrado= null;
+        foreach (var cadete in listadoCadetes)
+        {
+            if (cadete.Id == idCadete)
+            {
+                cadeteEncontrado= cadete;
+                break;
+            }
+        }
+        if (cadeteEncontrado != null)
+        {
+            listadoCadetes.Remove(cadeteEncontrado);
+        }
+    }
+    public void generarInforme(){
+        foreach (var cadete in listadoCadetes)
+        {
+            Console.WriteLine($"Cadete: {cadete.Nombre}, Pedido Entregados: {cadete.ListaPedido.Count}, Jornel: {cadete.jornalACobrar()}");W
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
